8402385abe79f0f3fb13ba29fcbe4c33cb5f4d19
SaulBgitgrub <saulbrito342@gmail.com>
2026-02-12 15:34:05 +0000

    Change of plan

--- Main.py
+++ Main.py
@@ -1,28 +1,34 @@
 #example = ['a', 'b', 'c', 'd', 'e']
 #print(random.choice(example)) - credits to stackoverflow.com
-import random
-#p stands for preset
-def pbank_process():
-    pword = random.choice(presetwb)
+
 def menu():
-    print("To begin the program input a number in order to access the Vocabulary Quiz Generator!\n Type 1 in order to access a preset word bank.\n", "Type 2 in order to enter words in.\n", "Type 3 in order to access the custom word bank.\n", "Type 4 if you need help understanding how the quiz works.\n", "Type 5 to exit the program")
-    print("-")*30
-
+    print("To begin the program input a number in order to access the Math Practice Tool!\n+ Type 1 in order to access the custom word bank.\n+ Type 2 if you need help understanding how the program works.\n+ Type 3 to exit the program\n-----")
 def exit():
-    
+    pass
+def c4():
+    print("-----\nThe code functions by giving the definition of the word and then prompting the user to input what word they believe it is.\nTyping 'exit' brings user to back to the main menu.\n -----")
+    return menu()
+def divpro(x,y):
+    total = x/y
+    return total
+def process():
+    decision = input("Which form of equation would you like to use: ")
+    if decision == "/":
+        o = int(input("What is your first number you would like to divide: "))
+        p = int(input("What is your second number you would like to divide: "))
+        divpro(o,p)
 
-    
-custombank = []
-presetwb = ["House","Tree","Shop","Planet","River","Time", "Year", "Person", "Highway", "Day", "Man"]
-def fullReset():
-    while len(custombank) > 0:
-        custombank.pop
 
+
+
+
+
+menu()
 choice = input("Type Here: ")
 if choice == "1":
-    pbank_process
+    print(process())
+elif choice == "2":
+    c4()
+    choice = input("Type Here: ")
 elif choice == "3":
-
-elif choice == "4":
-    print("The code functions by giving the definition of the word and then prompting the user to input what word they believe it is.\nTyping 'exit' brings user to back to the main menu")
-    
+    print(" ")
