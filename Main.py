cf6c1c1b4ec1244034d39d2688aa6b9d8392f7ad
SaulBgitgrub <saulbrito342@gmail.com>
2026-02-18 14:55:04 +0000

    Plain cake build

--- Main.py
+++ Main.py
@@ -1,34 +1,74 @@
-#example = ['a', 'b', 'c', 'd', 'e']
-#print(random.choice(example)) - credits to stackoverflow.com
+import sys
+#Credits to https://www.janbasktraining.com/ for the information regarding sys.exit()
 
 def menu():
-    print("To begin the program input a number in order to access the Math Practice Tool!\n+ Type 1 in order to access the custom word bank.\n+ Type 2 if you need help understanding how the program works.\n+ Type 3 to exit the program\n-----")
+    return print("To begin the program input a number in order to access the Math Practice Tool!\n+ Type 1 in order to access the calculator.\n+ Type 2 if you need help understanding how the program works.\n+ Type 3 to exit the program\n-----")
+def sel(x):
+    if x == "1":
+        process()
+    elif x == "2":
+        c2()
+        realmenu()
+    elif x == "3":
+        print("Thank you for your time!")
+        #sys.exit() simply stops the entire program
+        sys.exit()
+    else:
+        print("Invalid expression")
+    return realmenu()
 def exit():
     pass
-def c4():
-    print("-----\nThe code functions by giving the definition of the word and then prompting the user to input what word they believe it is.\nTyping 'exit' brings user to back to the main menu.\n -----")
-    return menu()
+def c2():
+    print("-----\nIn order to use the calculator, you just have to type in one of the following symbols ( -, +, *, or / ). \nThe user is returned to the main menu after a calculation or when they add an invalid expression.\n -----")
+    realmenu()
 def divpro(x,y):
     total = x/y
+    return total
+def timespro(x,y):
+    total = x*y
+    return total
+def addpro(x,y):
+    total = x+y
+    return total
+def minpro(x,y):
+    total = x-y
     return total
 def process():
     decision = input("Which form of equation would you like to use: ")
     if decision == "/":
-        o = int(input("What is your first number you would like to divide: "))
-        p = int(input("What is your second number you would like to divide: "))
-        divpro(o,p)
+        x = int(input("What is your first number you would like to divide: "))
+        y = int(input("What is your second number you would like to divide: "))
+        return divpro(x,y)
+    elif decision == "*":
+        x = int(input("What is your first number you would like to multiply: "))
+        y = int(input("What is your second number you would like to multiply: "))
+        return timespro(x,y)
+    elif decision == "+":
+        x = int(input("What is your first number you would like to add: "))
+        y = int(input("What is your second number you would like to add: "))
+        return addpro(x,y)
+    elif decision == "-":
+        x = int(input("What is your first number you would like to subtract: "))
+        y = int(input("What is your second number you would like to subtract: "))
+        return minpro(x,y)
+    else:
+        print("Invalid symbol")
+        realmenu()
+def realmenu():
+    menu()
+    x = input("Type Here: ")
+    sel(x)
 
+realmenu()
 
-
-
-
-
-menu()
-choice = input("Type Here: ")
-if choice == "1":
-    print(process())
-elif choice == "2":
-    c4()
-    choice = input("Type Here: ")
-elif choice == "3":
-    print(" ")
+# choice = input("Type Here: ")
+# if choice == "1":
+#     print(process())
+# elif choice == "2":
+#     c2()
+#     choice = input("Type Here: ")
+# elif choice == "3":
+#     print("Thank you for your time!")
+# else:
+#     print("Invalid expression")
+#     menu()
